086fedfbdde3fc72c847d4396aca25a1f7cae28f
Rafael Mafra <69654497+rafaelmaframg@users.noreply.github.com>
2022-04-17 16:37:35 +0100

    adicionado git ao porfolio

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,8 @@
-from django.conf.urls import include, url
+
 from django.urls import path
 from . import views
 
-#urlpatterns = [
-    #
-#]
 
 urlpatterns = [
-    #path('', views.index, name='index'),
-    path('', views.post_list),
+    path('', views.index),
 ]
--- blog/views.py
+++ blog/views.py
@@ -5,12 +5,6 @@ from .models import Post
 
 # Create your views here.
 
-
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
-
-
-def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    return render(request,'blog/index.html')
 
